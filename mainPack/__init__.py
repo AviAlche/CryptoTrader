584aa9120283683cd762a5fd673285414739044f
AviAlche <avi.altcheh@gmail.com>
2017-05-27 22:15:20 +0300

    bit2c fetcher get tickers done

--- mainPack/__init__.py
+++ mainPack/__init__.py
@@ -1,11 +1,14 @@
+from network.fetchers.Bit2cFetcher import Bit2cFetcher
 from network.fetchers.BitfinexFetcher import BitfinexFetcher
 from network.fetchers.PoloniexFetcher import PoloniexFetcher
 
 
 def main():
-    polo = PoloniexFetcher()
-    polo_data = polo.getPoloniexTicker()
-    bitfinex = BitfinexFetcher()
-    bitfinex_data = bitfinex.getBitfinexTicker()
+    # polo = PoloniexFetcher()
+    # polo_data = polo.getTickers()
+    # bitfinex = BitfinexFetcher()
+    # bitfinex_data = bitfinex.getBitfinexTicker()
+    bit2c = Bit2cFetcher()
+    bit2c_data = bit2c.getTickers()
 
 main()
---
+++ network/fetchers/Bit2cFetcher.py
@@ -0,0 +1,85 @@
+import hashlib
+import hmac
+import time
+import json
+import urllib
+
+
+class Bit2cFetcher:
+
+    _Url = "https://bit2c.co.il"
+    _EXCHANGES = "/Exchanges"
+    _ORDERS = "/Order"
+    _relevantPairs = {"BTN_NIS":0,
+                      "LTC_BTC":1,
+                      "LTC_NIS":2}
+
+    def __init__(self, Key = None, Secret = None):
+        self.Key = Key
+        self.Secret = Secret
+        self.nonce = int(time.time())
+
+
+    def query(self, command, req={}):
+
+        if(command == self._EXCHANGES):
+            ret = urllib.request.urlopen(urllib.request.Request(self._Url + self._EXCHANGES))
+            return json.loads(ret.read())
+        elif(command == "returnOrderBook"):
+            ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/public?command=' + command + '&currencyPair=' + str(req['currencyPair'])))
+            return json.loads(ret.read())
+        elif(command == "returnMarketTradeHistory"):
+            ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/public?command=' + "returnTradeHistory" + '&currencyPair=' + str(req['currencyPair'])))
+            return json.loads(ret.read())
+        else:
+            req['command'] = command
+            req['nonce'] = int(time.time()*1000)
+            post_data = urllib.request.urlencode(req)
+
+            sign = hmac.new(self.Secret, post_data, hashlib.sha512).hexdigest()
+            headers = {
+                'Sign': sign,
+                'Key': self.APIKey
+            }
+
+            ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/tradingApi', post_data, headers))
+            jsonRet = json.loads(ret.read())
+            return self.post_process(jsonRet)
+
+    # def balance(self):
+    #     qString = "nonce=" + str(self.nonce)
+    #     sign = self.ComputeHash(qString)
+    #     url = self.Url + "Account/Balance"
+    #     response = self.query(qString, url, sign)
+    #     _json = json.loads(response.decode("utf-8"))
+    #     # Balance(_json['BalanceNIS'], _json['BalanceLTC'], _json['BalanceBTC'])
+    #
+    # def getTrades(self, Pair, since=0, date=0):
+    #     url = self.Url + "Exchanges/" + str(Pair) + "/trades.json"
+    #     response = self.DownloadString(url)
+    #     _json = json.loads(response.decode("utf-8"))
+    #     ExchangesTrades = []
+    #     # for jsonObj in _json:
+    #         # exTrade = ExchangesTrade(jsonObj['date'], jsonObj['price'], jsonObj['amount'], jsonObj['tid'])
+    #         # ExchangesTrades.append(exTrade)
+    #     return ExchangesTrades
+
+    def getTicker(self, Pair=_relevantPairs["BTN_NIS"]):
+        ret = urllib.request.urlopen(urllib.request.Request(self._Url + self._EXCHANGES +"/"+ str(Pair) + "/Ticker.json"))
+        return json.loads(ret.read())
+        # _json = json.loads(response.decode("utf-8"))
+        # return Ticker(_json['h'], _json['l'], _json['ll'], _json['a'], _json['av'])
+
+    def getTickers(self):
+        ticker={}
+        for pairName, pairNum in self._relevantPairs.items():
+            ticker[pairName]=self.getTicker(pairNum)
+        return ticker
+
+
+
+
+
+
+
+
